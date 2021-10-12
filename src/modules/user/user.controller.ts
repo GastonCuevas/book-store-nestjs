db8bd0583884bc69ea9f130ac54917a3b8fcf086
GastonCuevas <gaston.cuevas@INFOCORP1.onmicrosoft.com>
2021-10-12 15:48:45 -0300

    [ADD] Role decorator and guard added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -10,7 +10,8 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { UserDto } from './dto/user.dto';
+import { RoleGuard } from '../guards/role.guard';
+import { Roles } from '../role/decorators/role.decorator';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 
@@ -19,12 +20,15 @@ export class UserController {
   constructor(private readonly _userService: UserService) {}
 
   @Get(':id')
-  async getUser(@Param('id', ParseIntPipe) id: number) {
+  @Roles('Administrador', 'GENERAL')
+  @UseGuards(AuthGuard(), RoleGuard)
+  async getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
     const user = await this._userService.get(id);
     return user;
   }
 
-  //@UseGuards(AuthGuard()) El AuthGuard hace que sea necesario utilizar el token bearer para acceder a la ruta
+  //El AuthGuard hace que sea necesario utilizar el token bearer para acceder a la ruta
+  @UseGuards(AuthGuard())
   @Get()
   async getUsers() {
     const users = await this._userService.getAll();
